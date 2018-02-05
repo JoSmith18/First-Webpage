fe7de763cfcb6f3f3a4eab49d0a7129dfaba0a7e
Jo'Tavious Smith <jsmith@basecampcodingacademy.org>
2018-02-05 14:17:22 -0600

    Made design changes by using Javascript to load and change data

---
+++ javascript.js
@@ -0,0 +1,143 @@
+PROJECTDATA = {
+    Ajax: {
+        title: 'November: AJAX Front-End',
+        url: 'https://github.com/JoSmith18/AJAX-Chirper-Frontend',
+        details:
+            'This was a cumulative project to showcase my skills in HTML, CSS, JavaScript, jQuery, Bootstrap, and Fontawesome. I was provided a backend by the instructor and had to create a twitter like application.',
+        imgurl: 'photos/Screenshot from 2017-12-15 13-55-31.png'
+    },
+
+    Minesweeper: {
+        title: 'November: Minesweeper',
+        url: 'https://github.com/JoSmith18/Minesweeper',
+        details:
+            'This is a browser based version of the classic game Minesweeper. I added my own touch to it but keep the same properties of the real game.',
+        gameurl:
+            'https://rawgit.com/JoSmith18/Minesweeper/master/dist/index/index.html',
+        imgurl: 'photos/minesweeper.png'
+    },
+    Holiday: {
+        title: 'October: Holiday Game',
+        url: 'https://github.com/JoSmith18/Halloween-Matching-Game',
+        details:
+            'This is a browser based game using JavaScript and jquery along with HTML and CSS. The user click on groups of matching pictures and score goes up accordingly.',
+        gameurl:
+            'https://rawgit.com/JoSmith18/Halloween-Matching-Game/master/dist/index/index.html',
+        imgurl: 'photos/holiday.png'
+    },
+    BrowserGladiator: {
+        title: 'October: Browser Anime-Gladiator-Game',
+        url: 'https://github.com/JoSmith18/JSgladiator',
+        details:
+            'This is a browser based version of my previous Anime-Gladiator-Game using JavaScript, Jquery, CSS, and HTML.',
+        gameurl:
+            'https://rawgit.com/JoSmith18/JSgladiator/master/dist/index/index.html',
+        imgurl: 'photos/glad.png'
+    },
+
+    TicTacToe: {
+        title: 'September: Tic-Tac-Joe',
+        url: 'https://github.com/JoSmith18/Tic-Tac-Joe',
+        details:
+            'For this application I used python and flask to remake the classic game of Tic Tac Toe. The game is in the browser.',
+        gameurl: 'photos/Peek 2017-09-29 13-14.mp4',
+        imgurl: 'photos/Screenshot from 2017-09-29 13-18-02.png'
+    },
+    Knowledge: {
+        title: 'September: Movie and Music Knowledge Tester',
+        url: 'https://github.com/JoSmith18/Movie_and_Music_Knowledge',
+        details:
+            'For this application I used <abbr title="Object-Oriented-Programming">OOP</abbr> and python to create a terminal based game that gives the user a song or movie and ask for either the artist or actor.',
+        gameurl: 'https://asciinema.org/a/hGOdpbuD2JTcnzdj75ivBEYiQ',
+        imgurl: 'https://asciinema.org/a/hGOdpbuD2JTcnzdj75ivBEYiQ.png'
+    },
+    Tetris: {
+        title: 'September: Tetris',
+        details:
+            'This was an application given to me mostly done by our teacher. My job was to fix some functions in the core and have the test coverage 100% for it. After completing what was necessary I added features like having it speed up the more the game was played and different colored blocks.',
+        url: 'photos/Peek 2017-09-20 15-00.mp4',
+        imgurl: 'photos/2017-09-20_15-46-30.png'
+    },
+    FreeForAll: {
+        title: 'August: Python Anime-Gladiator-Game',
+        url:
+            'https://github.com/JoSmith18/Modified-Free-For-All-Gladiator-Game',
+        details:
+            'This is a terminal based game using Object-Oriented-Programming and python. For this game I had a vast amount of characters in a txt file and read them in using the disk. In the core I had classes to get the type of fighter and for the actions that go on in battle.',
+        gameurl: 'https://asciinema.org/a/cW9npIgoeWIQiOxKcimt5uMFo',
+        imgurl: 'https://asciinema.org/a/cW9npIgoeWIQiOxKcimt5uMFo.png'
+    },
+    RentalStore: {
+        title: 'July: Rental-Store',
+        url: 'https://github.com/JoSmith18/rental_store_Jo-TaviousSmith',
+        details:
+            'This application was the accumulation of all the knowledge of Python I knew at the time. This project was a test of our python skills. We had to create a rental program in the language python that allows the user to be able to rent and return the items that are being rented.',
+        gameurl: 'https://asciinema.org/a/v6PZPhhENambxD65NIewyZf35',
+        imgurl: 'https://asciinema.org/a/v6PZPhhENambxD65NIewyZf35.png'
+    },
+    RecordKeeper: {
+        title: 'July:Record-Keeper',
+        url: 'https://github.com/Sfreeman99/Python-Record-Keeper',
+        details:
+            'This application is in python and allows the user to keep track of the number of wins, losses, and ties for anyone. I worked together on this application with a class-mate',
+        imgurl: 'https://asciinema.org/a/RfnGVpiImNlozmGR6n3flujV3.png'
+    },
+    StudentCheckin: {
+        title: 'July: Student Check-In Application',
+        url: 'https://github.com/JoSmith18/Student_Check-Ins',
+        details:
+            'This application is in python and has two mains. If user is a student then they will be taken to a place that allows them to check in and it records the time and day. If the user is an administrator then the have the option to check the times of each student or for a day.',
+        imgurl: 'https://asciinema.org/a/5VIzXx25bFZOZhwTcP0vI2Exa.png'
+    },
+    GasPump: {
+        title: 'July: Gas Pump Application',
+        url: 'https://github.com/JoSmith18/GasPump',
+        details:
+            'This application in python simulates getting gas from a gas pump. The application keeps track of the inventory and revenue made. Their are options for the user and an employee.',
+        imgurl: 'https://asciinema.org/a/0YfUIhw2ry4HXDmAJ6WAEaWRw.png'
+    }
+};
+
+function loadProjectData(key) {
+    var v = PROJECTDATA[key];
+    var id = '#' + key;
+    $(id)
+        .removeClass('notSelected')
+        .addClass('btn-success');
+    $('#project-title').html(v.title);
+    $('#project-details').html(v.details);
+    $('#project-url').attr('href', v.url);
+    $('#project-pic').attr('src', v.imgurl);
+    $('#gameurl').attr('href', v.gameurl);
+}
+
+function loadButtons() {
+    var keys = Object.keys(PROJECTDATA);
+    var buttons = keys.map(function(key) {
+        return (
+            '<div class="col-lg-4"><button class="notSelected project-button" id="' +
+            key +
+            '">' +
+            PROJECTDATA[key].title +
+            '</button></div>'
+        );
+    });
+
+    $('#all-project-buttons').html(buttons.join(''));
+}
+
+function main() {
+    loadButtons();
+
+    $('.project-button').on('click', function() {
+        var id = $(this).attr('id');
+        $('.project-button')
+            .removeClass('btn-success')
+            .addClass('notSelected');
+        loadProjectData(id);
+    });
+
+    loadProjectData('Ajax');
+}
+
+$(main);
